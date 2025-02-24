c200a2891d30fc527b5102d6ea88279111299265
Camila Kfouri <kfouri.camila@gmail.com>
2025-02-24 11:38:19 +0000

    modulos components e angular-material adicionados

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { AngularMaterialModule } from "../angular-material/angular-material.nodule";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    AngularMaterialModule,
+  ],
+  exports: []
+})
+export class ComponentsModule { }
